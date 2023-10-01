ac79a3d75d2bc07ae793a767709a387eaa0b1411
Priyansh-14 <priyanshja2003@gmail.com>
2023-10-01 23:53:55 +0530

    added comments

--- public/js/app.js
+++ public/js/app.js
@@ -1,37 +1,34 @@
- // Check if the user is logged in (you can replace this with your actual logic)
- const isLoggedIn = sessionStorage.getItem('isLoggedIn');
+const isLoggedIn = sessionStorage.getItem("isLoggedIn");
 
- // Function to display the user profile and logout options
- function showUserProfile(username) {
-   document.getElementById('user-profile').style.display = 'block';
-   document.getElementById('username').textContent = username;
-   document.getElementById('logout').style.display = 'block';
-   document.getElementById('login').style.display = 'none';
- }
+// Function to display the user profile and logout options
+function showUserProfile(username) {
+  document.getElementById("user-profile").style.display = "block";
+  document.getElementById("username").textContent = username;
+  document.getElementById("logout").style.display = "block";
+  document.getElementById("login").style.display = "none";
+}
 
- // Function to hide the user profile and logout options
- function hideUserProfile() {
-   document.getElementById('user-profile').style.display = 'none';
-   document.getElementById('logout').style.display = 'none';
-   document.getElementById('login').style.display = 'block';
- }
+// Function to hide the user profile and logout options
+function hideUserProfile() {
+  document.getElementById("user-profile").style.display = "none";
+  document.getElementById("logout").style.display = "none";
+  document.getElementById("login").style.display = "block";
+}
 
- // Check if the user is logged in and display the profile or login accordingly
- if (isLoggedIn) {
-   const username = sessionStorage.getItem('username');
-   showUserProfile(username);
- } else {
-   hideUserProfile();
- }
+// Check if the user is logged in and display the profile or login accordingly
+if (isLoggedIn) {
+  const username = sessionStorage.getItem("username");
+  showUserProfile(username);
+} else {
+  hideUserProfile();
+}
 
- // Logout functionality
- document.getElementById('logout-link').addEventListener('click', () => {
-   sessionStorage.removeItem('isLoggedIn');
-   sessionStorage.removeItem('username');
-   hideUserProfile();
- });
-
-
+// Logout functionality
+document.getElementById("logout-link").addEventListener("click", () => {
+  sessionStorage.removeItem("isLoggedIn");
+  sessionStorage.removeItem("username");
+  hideUserProfile();
+});
 
 // JavaScript to hide and show navbar on scroll
 let prevScrollPos = window.pageYOffset;
@@ -64,14 +61,10 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   });
 });
 
-const backToHomeButton = document.getElementById('backToHome');
+const backToHomeButton = document.getElementById("backToHome");
 
-backToHomeButton.addEventListener('click', () => {
-    window.location.href = 'index.html';
+backToHomeButton.addEventListener("click", () => {
+  window.location.href = "index.html";
 });
 
 //javascript to add user profile on login
-
-
- 
-
